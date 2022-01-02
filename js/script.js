d23e9a03906f29d6d7ec106781008e6d1f932dbc
Jakub Szczechowicz <jakub.szczechowicz@gmail.com>
2022-01-02 14:14:10 +0100

    js refactoring

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-const pln_seed = { pln: 1, eur: 0.22, usd: 0.24, gbp: 0.18, czk: 5.45, rub: 18, jpy: 27.59, btc: 0.00000515 };
-let fromAmount = document.querySelector(".js-fromAmount");
-let toAmount = document.querySelector(".js-toAmount");
-let fromCurrency = document.querySelector(".js-fromCurrency");
-let toCurrency = document.querySelector(".js-toCurrency");
-let formElement = document.querySelector(".js-form");
+{
+    const pln_seed = { pln: 1, eur: 0.22, usd: 0.24, gbp: 0.18, czk: 5.45, rub: 18, jpy: 27.59, btc: 0.00000515 };
+    let inputCurrency = document.querySelector(".js-fromCurrency");
+    let outputCurrency = document.querySelector(".js-toCurrency");
+    let fromAmount = document.querySelector(".js-fromAmount");
+    let toAmount = document.querySelector(".js-toAmount");
 
-formElement.addEventListener("input", () => {
-    calculate();
+    function reverse() {
+        let reversedFromCurrency = outputCurrency.value;
+        outputCurrency.value = inputCurrency.value;
+        inputCurrency.value = reversedFromCurrency;
+        calculate(inputCurrency, outputCurrency, fromAmount, toAmount, pln_seed);
+    }
 
-})
+    const liveFeedback = (inputCurrency, outputCurrency, fromAmount, toAmount, seed) => {
+        const formElement = document.querySelector(".js-form");
 
-function calculate() {
-    let inputCurrency = fromCurrency.value;
-    let outputCurrency = toCurrency.value;
+        formElement.addEventListener("input", () => {
+            calculate(inputCurrency, outputCurrency, fromAmount, toAmount, seed);
+        })
+    }
 
-    //Following formula was constructed to avoid setting up separate ratio objects for every currency - legit
+    const calculate = (inputCurrency, outputCurrency, fromAmount, toAmount, seed) => {
+        toAmount.value = (outputCurrency.value === "btc") ? (fromAmount.value * (seed[outputCurrency.value] / seed[inputCurrency.value])) : (fromAmount.value * (seed[outputCurrency.value] / seed[inputCurrency.value])).toFixed(2);
+    }
 
-    toAmount.value = (outputCurrency === "btc") ? (fromAmount.value * (pln_seed[outputCurrency] / pln_seed[inputCurrency])) : (fromAmount.value * (pln_seed[outputCurrency] / pln_seed[inputCurrency])).toFixed(2);
-}
+    const init = () => {
+        console.log("Hello fellow developers!")
+        liveFeedback(inputCurrency, outputCurrency, fromAmount, toAmount, pln_seed);
+    }
 
-function reverse() {
-    let reversedFromCurrency = toCurrency.value;
-    toCurrency.value = fromCurrency.value;
-    fromCurrency.value = reversedFromCurrency;
-    calculate();
+    init()
 }
